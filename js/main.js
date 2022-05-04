007daa862fcfd651bf92de8eb0de8282d95f1449
Jetsuko <jessean.infiltrator@gmail.com>
2022-05-03 17:00:38 -0700

    completed first draft of portfolio site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+const PROJECT_SECTIONS = Array.from(document.querySelectorAll('.projects'));
 
+window.addEventListener('DOMContentLoaded', e => {
+    const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+
+    const options = {
+        root: document.querySelector('#observer'),
+        threshold: 0.3,
+        rootMargin: '0px 0px -60% 0px'
+    }
+    
+    const navLinksObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const target = entry.target.id;
+                const targetLink = document.querySelector(`a[href="#${target}"]`);
+                document.querySelector('.nav-link-active').classList.remove('nav-link-active');
+                targetLink.classList.add('nav-link-active');
+            }
+        });
+    }, options);
+
+    PROJECT_SECTIONS.forEach(section => {
+        navLinksObserver.observe(section);
+    });
+})
